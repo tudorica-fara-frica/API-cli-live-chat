6bf88203b8415127601e9530ef19d54e60e81f40
Rîmbu Tudor-Gabriel <rtudorg2003@gmail.com>
2025-03-19 21:43:21 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const { Server } = require("socket.io");
+const http = require("http");
+
+const server = http.createServer();
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User connected!`);
+
+  socket.on("joinRoom", ({ user, room }) => {
+    socket.user = user;
+    socket.room = room;
+    socket.join(room);
+    io.to(socket.room).emit("userJoined", { user, room });
+  });
+
+  socket.on("chatMessage", (msg) => {
+    io.to(socket.room).emit("chatMessage", {
+      sender: socket.id,
+      msg,
+      user: socket.user,
+    });
+  });
+
+  socket.on("leaveRoom", () => {
+    io.to(socket.room).emit("leaveRoom", { user: socket.user });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected!");
+    if (socket.room) {
+      io.to(socket.room).emit("leaveRoom", { user: socket.user });
+    }
+  });
+});
+
+server.listen(3000, () => {
+  console.log(`Server is up and running on port 3000!`);
+});
