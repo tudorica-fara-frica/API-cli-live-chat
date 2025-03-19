a3307023583a8884da9a864600a551552eb0bfec
Rîmbu Tudor-Gabriel <rtudorg2003@gmail.com>
2025-03-19 22:04:58 +0200

    fixed some bugs

--- index.js
+++ index.js
@@ -39,6 +39,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
+server.listen(process.env.PORT || 3000, "0.0.0.0,", () => {
   console.log(`Server is up and running on port 3000!`);
 });
